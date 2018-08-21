048768541d1c8c53b0e9b5ef6cb1071bc42f2092
barneyman <barnaby.james.flint@gmail.com>
2018-08-21 14:13:13 +1000

    added my HA class

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
+
 setup(name='bjfGoogle',
       version='1.0',
       py_modules=['bjfGoogle'],
+      )
+
+setup(name='bjfHA',
+      version='1.0',
+      py_modules=['bjfHA'],
       )
