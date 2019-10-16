efcedf81510a101b2301ef9d223bcd7946185b47
barneyman-work <b.flint@sensysgatso.com>
2019-10-17 08:21:42 +1100

    added more deps in setup

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from distutils.core import setup
 setup(name='bjfGoogle',
       version='1.0',
       py_modules=['bjfGoogle'],
+	  depends=['google-api-python-client', 'google-auth-httplib2', 'google-auth-oauthlib', 'pathlib']
       )
 
-setup(name='bjfHA',
-      version='1.0',
-      py_modules=['bjfHA'],
-      )
